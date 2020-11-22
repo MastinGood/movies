afee3b0c22b9cd0f1a57440b59f1587aa3e48fe8
Jhon Kenrick <jhonkenrick28@gmail.com>
2020-11-23 00:21:39 +0800

    update2

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,34 @@
 <template>
   <div>
     <main class="dark2 w-full md:mt-20 mt-16 pb-20">
-      <VueSlickCarousel :arrows="true" :dots="true" :autoplay="true" :fade="true" :swipe="true" :touchMove="true">
-        <div v-for="(trending, index) in trendings.slice(0,10)" :key="index" class="slider w-full focus:outline-none relative">
-          <img :src="backdrop_path+trending.backdrop_path" class="slide-img object-cover object-top h-full w-full focus:outline-none">
-          <div class="xl:w-4/12 lg:w-5/12 md:w-6/12 sm:w-6/12 w-7/12 absolute bottom-0 xl:ml-40 lg:ml-24 md:ml-20 sm:ml-8 ml-6 lg:mb-24 md:mb-10 sm:mb-10 mb-8">
-            <div class="relative">
-              <h1 class="text-white lg:text-5xl md:text-4xl sm:text-4xl text-2xl font-semibold leading-none py-2">{{trending.title}}</h1>
-              <p class="text-white inline-block lg:text-md md:text-sm text-xs font-medium lg:pr-4 md:pr-3 pr-2">
-                 Rating : 
-                <!-- <svg class="h-6 w-6 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-              </svg> -->
-             <span class="primary">{{trending.vote_average * 10}}% </span>
-              </p>
-              <p class="text-white inline-block lg:text-md md:text-sm text-xs font-medium lg:pr-4 md:pr-3 pr-2">
-                 Popularity : 
-                <!-- <svg class="h-6 w-6 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-              </svg> -->
-             <span class="primary">{{trending.popularity}} </span>
-              </p>
-              <p class="md:block hidden text-white pt-2">{{trending.overview | truncate(200, '...')}}</p>
+      <client-only>
+        <VueSlickCarousel :arrows="true" :dots="true" :autoplay="true" :fade="true" :swipe="true" :touchMove="true">
+          <div v-for="(trending, index) in trendings.slice(0,10)" :key="index" class="slider w-full focus:outline-none relative">
+            <img :src="backdrop_path+trending.backdrop_path" class="slide-img object-cover object-top h-full w-full focus:outline-none">
+            <div class="xl:w-4/12 lg:w-5/12 md:w-6/12 sm:w-6/12 w-7/12 absolute bottom-0 xl:ml-40 lg:ml-24 md:ml-20 sm:ml-8 ml-6 lg:mb-24 md:mb-10 sm:mb-10 mb-8">
+              <div class="relative">
+                <h1 class="text-white lg:text-5xl md:text-4xl sm:text-4xl text-2xl font-semibold leading-none py-2">{{trending.title}}</h1>
+                <p class="text-white inline-block lg:text-md md:text-sm text-xs font-medium lg:pr-4 md:pr-3 pr-2">
+                   Rating : 
+                  <!-- <svg class="h-6 w-6 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                </svg> -->
+               <span class="primary">{{trending.vote_average * 10}}% </span>
+                </p>
+                <p class="text-white inline-block lg:text-md md:text-sm text-xs font-medium lg:pr-4 md:pr-3 pr-2">
+                   Popularity : 
+                  <!-- <svg class="h-6 w-6 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                </svg> -->
+               <span class="primary">{{trending.popularity}} </span>
+                </p>
+                <p class="md:block hidden text-white pt-2">{{trending.overview | truncate(200, '...')}}</p>
+              </div>
             </div>
+           <div class="dark-gradient"></div>
           </div>
-         <div class="dark-gradient"></div>
-        </div>
-      </VueSlickCarousel>
+        </VueSlickCarousel>
+      </client-only>
       <div class="md:w-10/12 w-11/12 mx-auto md:pt-16 pt-8">
         <div>
           <label class="md:text-lg text-md primary font-bold uppercase tracking-wider">Trending Movies</label>
